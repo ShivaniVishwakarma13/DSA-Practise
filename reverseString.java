8180afa4c71bec8e7a3e958d8d27f414218d7de6
Shivani Vishwakarma <141990431+ShivaniVishwakarma13@users.noreply.github.com>
2024-01-10 23:26:24 +0530

    update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,1 +1,20 @@
-System.out.println("Hello, ");
+import java.io.*;
+import java.util.Scanner;
+ 
+class ReverseString {
+    public static void main (String[] args) {
+       
+        String str= "Hello", newstr="";
+        char ch;
+       
+      System.out.println("Original word: "+str);
+     
+       
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i); 
+        newstr= ch+newstr;
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+}
